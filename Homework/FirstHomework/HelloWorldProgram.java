e1b23b8c9647038d6c760b1e579366e16ac150be
Sam <pyntia.vladyslav@gmail.com>
2020-10-15 12:03:03 +0300

    initial commit

---
+++ Homework/FirstHomework/HelloWorldProgram.java
@@ -0,0 +1,9 @@
+ public class HelloWorldProgram {
+        public static void main(String[] args) {
+
+            System.out.println("Hello Everyone.");
+            System.out.println("I`m Vlad. I'm so glad to have such wonderful and dear friends.");
+            System.out.println("See you on Saturday.");
+
+        }
+}
---
+++ Homework/FirstHomework/YourInitials.java
@@ -0,0 +1,22 @@
+public class YourInitials {
+    public static void main(String[] args) {
+
+        System.out.println("My initials are: PV");
+        System.out.println();
+        System.out.println("           PPPPPP        VVV                 VVV  ");
+        System.out.println("           PPP   PPP      VVV               VVV   ");
+        System.out.println("           PPP     PPP     VVV             VVV    ");
+        System.out.println("           PPP     PPP      VVV           VVV     ");
+        System.out.println("           PPP     PPP       VVV         VVV      ");
+        System.out.println("           PPP    PPP         VVV       VVV       ");
+        System.out.println("           PPPPPP              VVV     VVV        ");
+        System.out.println("           PPP                  VVV   VVV         ");
+        System.out.println("           PPP                   VVV VVV          ");
+        System.out.println("           PPP                    VVVVV           ");
+        System.out.println("           PPP                     VVV            ");
+        System.out.println("           PPP                     VVV            ");
+        System.out.println();
+
+
+    }
+}
